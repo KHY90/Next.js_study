807b151393cdaf19162619990d875cadbe43d2ba
KHY90 <power4206@gmail.com>
2025-02-19 01:33:21 +0900

    fetch 불러오기

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,26 @@
+"use client"
 
-export const metadata = {
-    title: 'Home'
-  }
+import { useEffect, useState } from "react";
 
-export default function Page(){
+export default function Page() {
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ movies, setMovies ] = useState();
+    const getMovies = async () => {
+        const response = await fetch(
+            "https://nomad-movies.nomadcoders.workers.dev/movies"
+            );
+        const json = await response.json();
+        setMovies(json);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
     return (
-    <div>
-        <h1>안녕</h1>
-    </div>
+        <div>
+            {isLoading ? "Loading..." : JSON.stringify(movies)}
+        </div>
     );
 }
