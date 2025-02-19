7558a40d633c90560b383b3fe31e092cbc06e217
KHY90 <power4206@gmail.com>
2025-02-20 01:43:00 +0900

    css 추가

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -8,19 +8,21 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
     const path = usePathname();
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>{path === "/" ? "🔥" : ""}
-                </li>
-                <li>
-                    <Link href="/about-us">About Us</Link>{path === "/about-us" ? "🔥" : ""}
-                </li>
-            </ul>
-        </nav>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <li>
+            <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
+          </li>
+          <li>
+            <Link href="/about-us">About Us</Link>
+            {path === "/about-us" ? "🔥" : ""}
+          </li>
+        </ul>
+      </nav>
     );
-}
+  }
