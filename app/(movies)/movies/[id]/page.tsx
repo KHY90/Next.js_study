3b9edcf9d49e611ff93e2f121a030ec3c6682680
KHY90 <power4206@gmail.com>
2025-02-20 02:04:11 +0900

    디테일 페이지 css

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -4,20 +4,17 @@ import MovieVideos from "../../../components/movie-videos";
 
 export default async function MovieDetail({
     params: { id },
-}: {
+  }: {
     params: { id: string };
-}) {
-
+  }) {
     return (
-        <div>
-            <h3>영화 디테일 페이지</h3>
-            <Suspense fallback={<h1>Loading 영화 정보</h1>}>
-                <MovieInfo id={id} />
-            </Suspense>
-            <h4>비디오</h4>
-            <Suspense fallback={<h1>Loading 비디오</h1>}>
-                <MovieVideos id={id} />
-            </Suspense>
-        </div>
+      <div>
+        <Suspense fallback={<h1>Loading movie info</h1>}>
+          <MovieInfo id={id} />
+        </Suspense>
+        <Suspense fallback={<h1>Loading movie videos</h1>}>
+          <MovieVideos id={id} />
+        </Suspense>
+      </div>
     );
-}
+  }
