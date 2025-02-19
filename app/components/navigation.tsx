1be51990ce56b31d69ec995b4a343cb413136c01
KHY90 <power4206@gmail.com>
2025-02-20 01:47:30 +0900

    css

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -11,18 +11,18 @@ import { usePathname } from "next/navigation";
 import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
-    const path = usePathname();
-    return (
-      <nav className={styles.nav}>
-        <ul className={styles.list}>
-          <li>
-            <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
-          </li>
-          <li>
-            <Link href="/about-us">About Us</Link>
-            {path === "/about-us" ? "🔥" : ""}
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+  const path = usePathname();
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About Us</Link>
+          {path === "/about-us" ? "🔥" : ""}
+        </li>
+      </ul>
+    </nav>
+  );
+}
